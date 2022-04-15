485fa9371861d3430eb922a7d26a32bf7fe4ce3b
Ender <endernull@icloud.com>
2022-04-15 13:06:33 +0200

    add cache and bugfixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    CacheModule.register({
+      ttl: 60 * 60 * 6,
+      max: 60 * 60 * 24 * 7,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,17 +1,29 @@
 import {
+  CACHE_MANAGER,
   HttpStatus,
+  Inject,
   Injectable,
   InternalServerErrorException,
+  Logger,
 } from '@nestjs/common';
+import { Cache } from 'cache-manager';
 import { TwitterApi } from 'twitter-api-v2';
 import { ErrorResponse, TweetMediaResults } from './app.interface';
 
 @Injectable()
 export class AppService {
+  private readonly logger = new Logger('TwitterDownloaderService');
+
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
+
   public async getMediaUrls(
     tweetId: string,
   ): Promise<TweetMediaResults | ErrorResponse> {
     try {
+      const cache: TweetMediaResults = await this.cacheManager.get(
+        `getMediaUrls/${tweetId}`,
+      );
+      if (cache) return cache;
       const twitterClient = new TwitterApi(process.env.TWITTER_CLIENT_TOKEN);
       const roClient = twitterClient.readOnly;
       const tweet = await roClient.v1.singleTweet(tweetId);
@@ -21,15 +33,26 @@ export class AppService {
         if (media.type === 'photo') {
           mediaUrls.push(media.media_url);
         } else if (media.type === 'video' || media.type === 'animated_gif') {
-          media.video_info.variants.every((variant) => {
-            if (variant.url.includes('.mp4')) {
-              mediaUrls.push(variant.url);
-              return false;
+          let url, bitrate;
+          media.video_info.variants.forEach((variant) => {
+            if (
+              variant.content_type === 'video/mp4' &&
+              (!bitrate || bitrate < variant.bitrate)
+            ) {
+              url = variant.url;
+              bitrate = variant.bitrate;
             }
           });
+          if (url) {
+            mediaUrls.push(url);
+          }
+        } else {
+          this.logger.log(`other media type: ${media.type}`);
         }
       });
-      return { tweetUrl, mediaUrls };
+      const response = { tweetUrl, mediaUrls };
+      await this.cacheManager.set(`getMediaUrls/${tweetId}`, response);
+      return response;
     } catch (exception) {
       throw new InternalServerErrorException(
         {
